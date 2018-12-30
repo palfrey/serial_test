9de11b0952e57174cbe6480e326a91373a39b9bc
Tom Parker-Shemilt <palfrey@tevp.net>
2018-12-30 10:25:35 +0000

    Add more advanced test

--- serial_test_test/src/lib.rs
+++ serial_test_test/src/lib.rs
@@ -1,9 +1,42 @@
 #[cfg(test)]
 mod tests {
     use serial_test_derive::serial;
+    use std::sync::Arc;
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use lazy_static::lazy_static;
+    use std::thread;
+
+    lazy_static! {
+        static ref LOCK: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
+    }
 
     #[test]
     #[serial(alpha)]
-    fn test_serial_attribute() {
+    fn test_serial_1() {
+        println!("Start 1");
+        LOCK.store(1, Ordering::Relaxed);
+        thread::sleep_ms(100);
+        println!("End 1");
+        assert_eq!(LOCK.load(Ordering::Relaxed), 1);
+    }
+
+    #[test]
+    #[serial(alpha)]
+    fn test_serial_2() {
+        println!("Start 2");
+        LOCK.store(2, Ordering::Relaxed);
+        thread::sleep_ms(200);
+        println!("End 2");
+        assert_eq!(LOCK.load(Ordering::Relaxed), 2);
+    }
+
+    #[test]
+    #[serial(alpha)]
+    fn test_serial_3() {
+        println!("Start 3");
+        LOCK.store(3, Ordering::Relaxed);
+        thread::sleep_ms(300);
+        println!("End 3");
+        assert_eq!(LOCK.load(Ordering::Relaxed), 3);
     }
 }
