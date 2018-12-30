186f60b7ef154f340f75a01368d2584002eed3f9
Tom Parker-Shemilt <palfrey@tevp.net>
2018-12-30 12:38:29 +0000

    Remove use of antidote

--- serial_test/src/lib.rs
+++ serial_test/src/lib.rs
@@ -1,8 +1,7 @@
-use antidote::{Mutex, RwLock};
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::ops::{Deref, DerefMut};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex, RwLock};
 
 lazy_static! {
     static ref LOCKS: Arc<RwLock<HashMap<String, Mutex<()>>>> =
@@ -12,17 +11,18 @@ lazy_static! {
 pub fn serial_core(name: &str, function: fn()) {
     // Check if a new key is needed. Just need a read lock, which can be done in sync with everyone else
     let new_key = {
-        let unlock = LOCKS.read();
+        let unlock = LOCKS.read().unwrap();
         !unlock.deref().contains_key(name)
     };
     if new_key {
         // This is the rare path, which avoids the multi-writer situation mostly
         LOCKS
             .write()
+            .unwrap()
             .deref_mut()
             .insert(name.to_string(), Mutex::new(()));
     }
-    let unlock = LOCKS.read();
+    let unlock = LOCKS.read().unwrap();
     // _guard needs to be named to avoid being instant dropped
     let _guard = unlock.deref()[name].lock();
     function();
