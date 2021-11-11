d06c8b2b50c3093734df31cbbebe24fa30120024
Tom Parker-Shemilt <palfrey@tevp.net>
2021-11-11 23:27:16 +0000

    Initial work on file-based locking

---
+++ serial_test/src/file_lock.rs
@@ -0,0 +1,102 @@
+use fslock::LockFile;
+use std::{env, fs, process, str::from_utf8};
+
+struct Lock {
+    lockfile: LockFile,
+    needs_unlock: bool,
+}
+
+impl Lock {
+    fn unlock(self: &mut Lock) {
+        if self.needs_unlock {
+            self.lockfile.unlock().unwrap();
+            println!("Unlock");
+        }
+    }
+}
+
+fn do_lock(path: &str) -> Lock {
+    let mut lockfile = LockFile::open(path).unwrap();
+    println!("Waiting on {:?}", path);
+    let pid_str = format!("{}", process::id());
+    let can_lock = lockfile.try_lock().unwrap();
+    if !can_lock {
+        let raw_file = fs::read(path).unwrap();
+        let get_pid = from_utf8(&raw_file).unwrap();
+        if get_pid != pid_str {
+            lockfile.lock().unwrap();
+            fs::write(path, pid_str).unwrap();
+        }
+    } else {
+        println!("Got lock for {:?}", path);
+        fs::write(path, pid_str).unwrap();
+    }
+    Lock {
+        lockfile,
+        needs_unlock: can_lock,
+    }
+}
+
+fn path_for_name(name: &str) -> String {
+    let mut pathbuf = env::temp_dir();
+    pathbuf.push(format!("serial-core-{}", name));
+    pathbuf.into_os_string().into_string().unwrap()
+}
+
+fn make_lock_for_name_and_path(name: &str, path: Option<&str>) -> Lock {
+    if let Some(opt_path) = path {
+        do_lock(opt_path)
+    } else {
+        let default_path = path_for_name(name);
+        do_lock(&default_path)
+    }
+}
+
+#[doc(hidden)]
+pub fn fs_serial_core(name: &str, path: Option<&str>, function: fn()) {
+    let mut lock = make_lock_for_name_and_path(name, path);
+    function();
+    lock.unlock();
+}
+
+#[doc(hidden)]
+pub fn fs_serial_core_with_return<E>(
+    name: &str,
+    path: Option<&str>,
+    function: fn() -> Result<(), E>,
+) -> Result<(), E> {
+    let mut lock = make_lock_for_name_and_path(name, path);
+    let ret = function();
+    lock.unlock();
+    ret
+}
+
+#[doc(hidden)]
+pub async fn fs_async_serial_core_with_return<E>(
+    name: &str,
+    path: Option<&str>,
+    fut: impl std::future::Future<Output = Result<(), E>>,
+) -> Result<(), E> {
+    let mut lock = make_lock_for_name_and_path(name, path);
+    let ret = fut.await;
+    lock.unlock();
+    ret
+}
+
+#[doc(hidden)]
+pub async fn fs_async_serial_core(
+    name: &str,
+    path: Option<&str>,
+    fut: impl std::future::Future<Output = ()>,
+) {
+    let mut lock = make_lock_for_name_and_path(name, path);
+    fut.await;
+    lock.unlock();
+}
+
+#[test]
+fn test_serial() {
+    fs_serial_core("test", None, || {
+        fs_serial_core("test", None, || {});
+    });
+}
--- serial_test/src/lib.rs
+++ serial_test/src/lib.rs
@@ -19,75 +19,17 @@
 //! Multiple tests with the [serial](attr.serial.html) attribute are guaranteed to be executed in serial. Ordering
 //! of the tests is not guaranteed however.
 
-use lazy_static::lazy_static;
-use parking_lot::ReentrantMutex;
-use std::collections::HashMap;
-use std::ops::{Deref, DerefMut};
-use std::sync::{Arc, RwLock};
+mod code_lock;
+mod file_lock;
 
-lazy_static! {
-    static ref LOCKS: Arc<RwLock<HashMap<String, ReentrantMutex<()>>>> =
-        Arc::new(RwLock::new(HashMap::new()));
-}
+pub use code_lock::{
+    async_serial_core, async_serial_core_with_return, serial_core, serial_core_with_return,
+};
 
-fn check_new_key(name: &str) {
-    // Check if a new key is needed. Just need a read lock, which can be done in sync with everyone else
-    let new_key = {
-        let unlock = LOCKS.read().unwrap();
-        !unlock.deref().contains_key(name)
-    };
-    if new_key {
-        // This is the rare path, which avoids the multi-writer situation mostly
-        LOCKS
-            .write()
-            .unwrap()
-            .deref_mut()
-            .insert(name.to_string(), ReentrantMutex::new(()));
-    }
-}
-
-#[doc(hidden)]
-pub fn serial_core_with_return<E>(name: &str, function: fn() -> Result<(), E>) -> Result<(), E> {
-    check_new_key(name);
-
-    let unlock = LOCKS.read().unwrap();
-    // _guard needs to be named to avoid being instant dropped
-    let _guard = unlock.deref()[name].lock();
-    function()
-}
-
-#[doc(hidden)]
-pub fn serial_core(name: &str, function: fn()) {
-    check_new_key(name);
-
-    let unlock = LOCKS.read().unwrap();
-    // _guard needs to be named to avoid being instant dropped
-    let _guard = unlock.deref()[name].lock();
-    function();
-}
-
-#[doc(hidden)]
-pub async fn async_serial_core_with_return<E>(
-    name: &str,
-    fut: impl std::future::Future<Output = Result<(), E>>,
-) -> Result<(), E> {
-    check_new_key(name);
-
-    let unlock = LOCKS.read().unwrap();
-    // _guard needs to be named to avoid being instant dropped
-    let _guard = unlock.deref()[name].lock();
-    fut.await
-}
-
-#[doc(hidden)]
-pub async fn async_serial_core(name: &str, fut: impl std::future::Future<Output = ()>) {
-    check_new_key(name);
-
-    let unlock = LOCKS.read().unwrap();
-    // _guard needs to be named to avoid being instant dropped
-    let _guard = unlock.deref()[name].lock();
-    fut.await
-}
+pub use file_lock::{
+    fs_async_serial_core, fs_async_serial_core_with_return, fs_serial_core,
+    fs_serial_core_with_return,
+};
 
 // Re-export #[serial].
 #[allow(unused_imports)]
