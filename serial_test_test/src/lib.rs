e01c8db7ca48e65d134d73d907c4ee40fd2fad65
Heinz N. Gies <heinz@licenser.net>
2022-07-25 09:28:55 +0200

    Try no timeout
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- serial_test_test/src/lib.rs
+++ serial_test_test/src/lib.rs
@@ -107,11 +107,12 @@ mod tests {
     use serial_test::{file_parallel, file_serial};
 
     #[test]
-    #[serial(timeout_key, timeout_ms = 60000)]
+    #[serial()]
     fn demo_timeout_with_key() {}
 
     #[test]
-    #[serial(timeout_ms = 60000)]
+    #[serial()]
+    // #[serial(timeout_ms = 60000)]
     fn demo_timeout() {}
 
     #[test]
